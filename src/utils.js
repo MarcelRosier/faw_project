16deb5aa1d06b31288405f8d5c8b0762e1fef0f7
Marcel Rosier <marcel.rosier@gmx.de>
2023-02-17 22:29:44 +0100

    - added functionl register/login/ logout flow

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
-function signIn() {
-  login_form = document.getElementById("login_form");
+function register() {
+  login_form = document.getElementById("register_form");
   if (!login_form.checkValidity()) {
     return;
   }
@@ -21,8 +21,37 @@ function signIn() {
 
   // set current user as active
   setActiveUser(user);
-  // nav back to index page
-  location.href = "/";
+}
+
+function login(event) {
+  login_form = document.getElementById("login_form");
+  if (!login_form.checkValidity()) {
+    return;
+  }
+
+  // get values
+  email_element = document.getElementById("email_input");
+  pw_element = document.getElementById("password_input");
+  error_element = document.getElementById("login_error_p");
+
+  // check if access data is valid
+  user_db = getUsers();
+  user = user_db.get(email_element.value);
+
+  if (user === undefined) {
+    event.preventDefault();
+    error_element.hidden = false;
+    return;
+  }
+  // check pw
+  if (user.password !== pw_element.value) {
+    event.preventDefault();
+    error_element.hidden = false;
+    return;
+  }
+
+  // set current user as active
+  setActiveUser(user);
 }
 
 function setActiveUser(user) {
