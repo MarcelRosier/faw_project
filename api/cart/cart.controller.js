060e869c7b602775beece6fd68a79a626280921b
Stefan <stefanlj1991@gmail.com>
2023-04-11 12:38:01 +0200

    Added DELETE product and GET cart

--- api/cart/cart.controller.js
+++ api/cart/cart.controller.js
@@ -1,6 +1,8 @@
 import { read, write } from './cart.model.js';
 import { fetchProductById } from '../products/products.controller.js';
 
+
+/* Create cart for {userId} */
 export function createCart(req, res) {
   const userId = req.params.userId;
 
@@ -24,6 +26,7 @@ export function createCart(req, res) {
   });
 }
 
+/* Add product to cart with {userId} */
 export async function addProduct(req, res) {
   const userId = req.params.userId;
   const productId = parseInt(req.params.productId);
@@ -62,4 +65,54 @@ export async function addProduct(req, res) {
   } catch (error) {
     res.status(500).send('Error adding product to cart');
   }
+}
+
+/* Delete product from cart with {userId} */
+export function removeProduct(req, res) {
+  const userId = req.params.userId;
+  const productId = parseInt(req.params.productId);
+
+  read((err, carts) => {
+    if (err) {
+      return res.status(500).send('Error reading cart file');
+    }
+
+    if (!carts[userId]) {
+      return res.status(404).send('Cart not found for this user');
+    }
+
+    const productIndex = carts[userId].findIndex((item) => item.id === productId);
+
+    if (productIndex === -1) {
+      return res.status(404).send('Product not found in cart');
+    }
+
+    carts[userId].splice(productIndex, 1);
+    write(carts, (err) => {
+      if (err) {
+        return res.status(500).send('Error writing to cart file');
+      }
+
+      res.status(200).send('Product removed from cart');
+    });
+  });
+}
+
+/* Get cart with {userId} */
+export function getCart(req, res) {
+  const userId = req.params.userId;
+
+  read((err, carts) => {
+    if (err) {
+      return res.status(500).send('Error reading cart file');
+    }
+
+    const cart = carts[userId];
+
+    if (!cart) {
+      return res.status(404).send('Cart not found for this user');
+    }
+
+    res.status(200).json(cart);
+  });
 }
--- api/cart/cart.route.js
+++ api/cart/cart.route.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import {
   createCart,
-   addProduct
+   addProduct,
+   removeProduct,
+   getCart
   } from './cart.controller.js';
 
 export const router = express.Router();
 
 router.post('/cart/:userId', createCart);
 router.post('/cart/:userId/:productId', addProduct);
-/* router.delete('/cart/:userId/:productId', removeProduct);
-router.get('/cart/:userId', getCart); */
+router.delete('/cart/:userId/:productId', removeProduct);
+router.get('/cart/:userId', getCart);
 
 export default router;
 
