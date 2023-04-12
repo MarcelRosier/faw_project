22b1f1e8c5b5dd2b2c8072c0a3f671aa2b3d86f8
Stefan <stefanlj1991@gmail.com>
2023-04-12 12:13:13 +0200

    Removed cart function. Added create cart functionality to addProduct. Added incremental quantity counter to addProduct. removed createCart route and endpoint.

--- api/cart/cart.controller.js
+++ api/cart/cart.controller.js
@@ -1,88 +1,61 @@
 import { read, write } from './cart.model.js';
 import { fetchProductById } from '../products/products.controller.js';
 
-
-/* Create cart for {userId} */
-export function createCart(req, res) {
-  const userId = req.params.userId;
-
-  read((err, carts) => {
-    if (err) {
-      return res.status(500).send('Error reading cart file');
-    }
-
-    if (carts[userId]) {
-      return res.status(400).send('Cart already exists for this user');
-    }
-
-    carts[userId] = [];
-    write(carts, (err) => {
-      if (err) {
-        return res.status(500).send('Error writing to cart file');
-      }
-
-      res.status(201).send('Cart created');
-    });
-  });
-}
-
-/* Add product to cart with {userId} */
 export async function addProduct(req, res) {
   const userId = req.params.userId;
-  const productId = parseInt(req.params.productId);
+  const { productId } = req.body;
 
   try {
     const product = await fetchProductById(productId);
 
-    if (!product) {
-      return res.status(404).send('Product not found');
-    }
-
     read((err, carts) => {
       if (err) {
-        return res.status(500).send('Error reading cart file');
+        return res.status(500).send('error reading cart');
       }
 
       if (!carts[userId]) {
-        return res.status(404).send('Cart not found for this user');
+        carts[userId] = [];
       }
 
-      const productIndex = carts[userId].findIndex((item) => item.id === product.id);
+    const existingCartItemIndex = carts[userId].findIndex(item => item.productId === product.id);
 
-      if (productIndex !== -1) {
-        return res.status(400).send('Product already in cart');
+      if (existingCartItemIndex !== -1) {
+        carts[userId][existingCartItemIndex].quantity += 1;
+      } else {
+        const cartItem = {
+          productId: product.id,
+          quantity: 1,
+        };
+        carts[userId].push(cartItem);
       }
 
-      carts[userId].push(product);
       write(carts, (err) => {
         if (err) {
-          return res.status(500).send('Error writing to cart file');
+          return res.status(500).send('Error adding to cart');
         }
 
-        res.status(201).send('Product added to cart');
+        res.status(201).send('product added to cart');
       });
     });
   } catch (error) {
-    res.status(500).send('Error adding product to cart');
+    res.status(500).send('Error getting product');
   }
 }
 
-/* Delete product from cart with {userId} */
 export function removeProduct(req, res) {
   const userId = req.params.userId;
-  const productId = parseInt(req.params.productId);
+  const { productId } = req.body;
 
   read((err, carts) => {
     if (err) {
-      return res.status(500).send('Error reading cart file');
+      return res.status(500).send('Error reading cart');
     }
 
     if (!carts[userId]) {
-      return res.status(404).send('Cart not found for this user');
+      return res.status(404).send('cart not found for user');
     }
 
-    const productIndex = carts[userId].findIndex((item) => item.id === productId);
-
+    const productIndex = carts[userId].findIndex(item => item.productId === productId);
     if (productIndex === -1) {
       return res.status(404).send('Product not found in cart');
     }
@@ -90,21 +63,20 @@ export function removeProduct(req, res) {
     carts[userId].splice(productIndex, 1);
     write(carts, (err) => {
       if (err) {
-        return res.status(500).send('Error writing to cart file');
+        return res.status(500).send('error adding to cart');
       }
 
-      res.status(200).send('Product removed from cart');
+      res.status(200).send('product removed from cart');
     });
   });
 }
 
-/* Get cart with {userId} */
 export function getCart(req, res) {
   const userId = req.params.userId;
 
   read((err, carts) => {
     if (err) {
-      return res.status(500).send('Error reading cart file');
+      return res.status(500).send('Error reading cart');
     }
 
     const cart = carts[userId];
--- api/cart/cart.route.js
+++ api/cart/cart.route.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import {
-  createCart,
    addProduct,
    removeProduct,
    getCart
@@ -8,9 +7,8 @@ import {
 
 export const router = express.Router();
 
-router.post('/cart/:userId', createCart);
-router.post('/cart/:userId/:productId', addProduct);
-router.delete('/cart/:userId/:productId', removeProduct);
+router.post('/cart/:userId', addProduct);
+router.delete('/cart/:userId', removeProduct);
 router.get('/cart/:userId', getCart);
 
 export default router;
