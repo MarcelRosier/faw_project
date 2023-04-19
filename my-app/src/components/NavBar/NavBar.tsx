4ce653115945eae298ec8ebe041afccc35f809e8
Marcel Rosier <marcel.rosier@gmx.de>
2023-04-19 20:52:11 +0200

    - navbar changes based on user loggedin/out

--- my-app/src/components/NavBar/NavBar.tsx
+++ my-app/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CurrentUserContext } from "../../App";
 
 export const NavBar = () => {
+  const { user, setUser } = useContext(CurrentUserContext);
   return (
     <header>
       <nav className="navbar navbar-light">
@@ -17,23 +19,33 @@ export const NavBar = () => {
         <a className="navbar navbar-dark ml-auto" href="/shop">
           Shop
         </a>
-        <a
-          className="navbar navbar-dark mr-1"
-          style={{ textAlign: "right" }}
-          href="/login"
-          id="login"
-        >
-          Login
-        </a>
-        <a className="navbar navbar-dark mr-1" href="/register" id="register">
-          Register
-        </a>
+        {user.id == -1 ? (
+          <>
+            <a
+              className="navbar navbar-dark mr-1"
+              style={{ textAlign: "right" }}
+              href="/login"
+              id="login"
+            >
+              Login
+            </a>
+            <a
+              className="navbar navbar-dark mr-1"
+              href="/register"
+              id="register"
+            >
+              Register
+            </a>
+          </>
+        ) : (
+          <a href="">Logout</a>
+        )}
         <a
           id="navbar-msg"
           className="navbar nav-item navbar-text"
           style={{ textAlign: "right" }}
         >
-          Welcome
+          {user.id == -1 ? "Welcome!" : `Welcome ${user.firstName}`}
         </a>
         <a href="/carts/1">
           <img
