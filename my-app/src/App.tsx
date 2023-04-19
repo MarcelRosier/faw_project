029530b158c34b66fe184263564c5f3893e16204
Marcel Rosier <marcel.rosier@gmx.de>
2023-04-19 13:56:28 +0200

    - reanemd details route

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,29 +1,27 @@
-
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Hero } from './components/Hero/Hero'
-import { Shop } from './components/Shop/Shop'
-import { Cart } from './components/Cart/Cart'
-import { ProductDetails } from './components/ProductDetails/ProductDetails';
-import { Register } from './components/Register/Register';
-import { Login } from './components/Login/Login';
+import { Hero } from "./components/Hero/Hero";
+import { Shop } from "./components/Shop/Shop";
+import { Cart } from "./components/Cart/Cart";
+import { ProductDetails } from "./components/ProductDetails/ProductDetails";
+import { Register } from "./components/Register/Register";
+import { Login } from "./components/Login/Login";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/carts/:id" element={<Cart/>} />
-      {/* <Route path="/checkout" element={<Checkout/>} /> */}
-      <Route path="/description/:id" element={<ProductDetails/>} />
-      <Route path="/register" element={<Register/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/" element={<Hero/>} />
-    </Routes>
-  </BrowserRouter>
-  )
+      <Routes>
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/carts/:id" element={<Cart />} />
+        {/* <Route path="/checkout" element={<Checkout/>} /> */}
+        <Route path="/details/:id" element={<ProductDetails />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Hero />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
